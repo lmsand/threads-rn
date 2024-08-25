7919442ad4b1412f022a803c02785ebbce37dba4
lmsand <lmsand99@icloud.com>
2024-08-25 01:56:30 -0500

    login screen

--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,14 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ToastAndroid,
+  Alert
+} from 'react-native';
+import React, {useState} from 'react';
 
-type Props = {}
+type Props = {
+  navigation: any;
+};
 
-const LoginScreen = (props: Props) => {
+const LoginScreen = ({navigation}: Props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e: any) => {
+    Alert.alert('Login successful')
+
+    ToastAndroid.showWithGravity(
+      'Login successful',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    );
+  };
+
   return (
-    <View className='flex-[1] items-center justify-center'>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <View className="flex-[1] items-center justify-center">
+      <View className="w-[70%]">
+        <Text className="text-[25px] font-[600] text-center">Login</Text>
+        <TextInput
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          className="w-full h-[35px] border border-[#343434] px-2 my-2"
+        />
 
-export default LoginScreen
+        <TextInput
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          className="w-full h-[35px] border border-[#343434] px-2 my-2"
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity className="mt-6">
+          <Text
+            className="w-full h-[40px] text-[#fff] text-center pt-[8px] text-[20px] bg-black"
+            onPress={submitHandler}>
+            Login
+          </Text>
+        </TouchableOpacity>
+
+        <Text className="pt-3" onPress={() => navigation.navigate('Signup')}>
+          Don't have an account? <Text>Sign up</Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
